48c1c572fc4d5d5d19d8118d91329dd683db903f
Siri Mykland <>
2024-09-02 15:53:25 +0200

    moderniser skript

--- installMacOSpackages.sh
+++ installMacOSpackages.sh
@@ -4,9 +4,13 @@
 echo "##################################"
 echo "            Homebrew              "
 echo "##################################"
-if test ! $(which brew); then
+if test ! "$(which brew)"; then
     echo "Installing homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/Siri.Mykland/.profile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+
 else
     # Update homebrew
     echo "Updating homebrew..." && brew update
@@ -22,23 +26,13 @@ apps=(
   rectangle
   postman
   google-chrome
-  slack
   spotify
   visual-studio-code
   intellij-idea
-  intellij-idea-ce
-  microsoft-teams
-  microsoft-outlook
-  1password
   vmware-horizon-client
   # Formulae
-  kotlin
   tmux
-  nvm
-  jenv
   mosh
-  awscli
-  AdoptOpenJDK/openjdk/adoptopenjdk{8,11,14}
 )
 
 
@@ -51,16 +45,16 @@ brew_exist() {
 }
 
 brew_install() {
-    if brew_exist $1; then
+    if brew_exist "$1"; then
         echo "${1} is already installed"
     else
         echo "\nInstalling $1"
-        brew install $1 && echo "\n"
+        brew install "$1" && echo "\n"
     fi
 }
 
-for app in ${apps[@]}; do
-  brew_install $app
+for app in "${apps[@]}"; do
+  brew_install "$app"
 done
 
 #Docker
@@ -70,32 +64,33 @@ brew install --cask docker
 # Post brew install
 brew cleanup
 
-# npm and node
+
 echo "\n##################################"
 echo "              Node                "
 echo "##################################"
 
-if test $(which node) && brew_exist node; then
+brew_install nvm
+
+(echo; echo 'export NVM_DIR="$HOME/.nvm"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion') >> ~/.profile
+
+source ~/.profile
+
+
+if test "$(which node)" && brew_exist node; then
     echo "Uninstalling Node..."
     brew uninstall --ignore-dependencies node 
     brew uninstall node 
 fi
-if test brew_exist nvm; then
-  nvm install-latest-npm && echo "Installed npm ${npm -v}"
-  nvm install 12 && echo "Installed node ${nvm list | grep v12 -m1}"
-  nvm install 14 && echo "Installed node ${nvm list | grep v14 -m1}"
+
+if brew_exist nvm; then
+  nvm install 18
+  nvm install 22 --default
+  echo "\nYou got these versions of node:"
+  nvm ls --no-alias
 fi
 
-# java
-echo "\n##################################"
-echo "              Java                "
-echo "##################################"
-if brew_exist jenv ; then 
-  jenv add /Library/Java/JavaVirtualMachines/adoptopenjdk-14.jdk/Contents/Home
-  jenv add /Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home
-  jenv add /Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
-  jenv global 11
-fi
 
 
 # oh-my-zsh
@@ -103,5 +98,9 @@ echo "\n##################################"
 echo "            Oh-My-Zsh             "
 echo "##################################"
 zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-if -e ~/.zshrc.pre-oh-my-zsh ; then mv ~/.zshrc.pre-oh-my-zsh  ~/.zshrc fi
+if -e ~/.zshrc.pre-oh-my-zsh ; then 
+  mv ~/.zshrc.pre-oh-my-zsh  ~/.zshrc 
+fi
+
+
 source ~/.zshrc
